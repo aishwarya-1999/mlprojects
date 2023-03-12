8f1dbfd73844294f5eb892afc8116a32bf48b586
aishwarya-1999 <ashuchaddd@gmail.com>
2023-03-12 13:34:28 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function returns requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+
+setup(
+name = "mlprojects",
+version = '0.0.1',
+author= 'Aishwarya',
+author_email='aishwaryachandra716@gmail.com',
+packages = find_packages(),
+install_requires = get_requirements('requirements.txt')
+)
